88d789f6b2e9b4689f2f985572c19940c5ce6cf7
jaskrrish <jashehz@gmail.com>
2024-07-29 19:12:27 +0530

    Set Navbar components

---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="bg-white shadow-lg">
+      <div className="container mx-auto px-6 py-3"></div>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ frontend/src/lib/constants.ts
@@ -0,0 +1,19 @@
+type NavLink = {
+  name: string;
+  path: string;
+};
+
+export const navLink: NavLink[] = [
+  {
+    name: "Home",
+    path: "hero",
+  },
+  {
+    name: "About",
+    path: "about",
+  },
+  {
+    name: "Contact",
+    path: "contact",
+  },
+];
