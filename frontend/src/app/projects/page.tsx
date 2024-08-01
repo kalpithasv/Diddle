4c19c079e85d166a1b8acc9977f17f09baa5fbe0
jaskrrish <jashehz@gmail.com>
2024-08-01 22:33:30 +0530

    chore: Update framer-motion npm dependency to version 11.3.21

--- frontend/src/app/projects/page.tsx
+++ frontend/src/app/projects/page.tsx
@@ -3,10 +3,11 @@ import Image from "next/image";
 import React from "react";
 import { CardBody, CardContainer, CardItem } from "@/components/ui/3d-card";
 import Link from "next/link";
+import { freelancePic } from "../../../public";
 
 const ProjectPage = () => {
   return (
-    <div className="w-screen h-screen flex gap-x-4 justify-center items-center">
+    <div className="w-screen h-screen flex flex-wrap gap-x-4 justify-center items-center">
       <ProjectCard />
       <ProjectCard />
       <ProjectCard />
@@ -35,7 +36,7 @@ function ProjectCard() {
         </CardItem>
         <CardItem translateZ="100" className="w-full mt-4">
           <Image
-            src="https://images.unsplash.com/photo-1441974231531-c6227db76b6e?q=80&w=2560&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            src={freelancePic}
             height="1000"
             width="1000"
             className="h-60 w-full object-cover rounded-xl group-hover/card:shadow-xl"
